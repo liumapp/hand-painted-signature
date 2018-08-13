aa35d71a8a14512a595df3fa02ed6421c4b484c7
liumapp <liumapp.com@gmail.com>
2018-08-13 14:53:46 +0800

    ready to test generate a qr code for open a handle write page

--- src/main/java/com/liumapp/spic/core/qr/GenerateQrCode.java
+++ src/main/java/com/liumapp/spic/core/qr/GenerateQrCode.java
@@ -1,6 +1,11 @@
 package com.liumapp.spic.core.qr;
 
+import com.alibaba.fastjson.JSONObject;
+import com.liumapp.qtools.http.HttpTool;
 import com.liumapp.spic.core.job.JobDetail;
+import com.liumapp.spic.core.qr.require.GenerateQrCodeRequire;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author liumapp
@@ -9,5 +14,19 @@ import com.liumapp.spic.core.job.JobDetail;
  * @homepage http://www.liumapp.com
  * @date 2018/8/13
  */
-public class GenerateQrCode extends JobDetail  {
+public class GenerateQrCode extends JobDetail <GenerateQrCodeRequire> {
+
+    private HttpTool httpTool;
+
+    private static Logger logger = LoggerFactory.getLogger(GenerateQrCode.class);
+
+    public GenerateQrCode () {
+        this.httpTool = new HttpTool();
+    }
+
+    @Override
+    public JSONObject handle(GenerateQrCodeRequire data) {
+        return null;
+    }
+
 }
---
+++ src/main/java/com/liumapp/spic/core/qr/require/GenerateQrCodeRequire.java
@@ -0,0 +1,16 @@
+package com.liumapp.spic.core.qr.require;
+
+import com.liumapp.spic.core.job.JobData;
+
+/**
+ * @author liumapp
+ * @file GenerateQrCodeRequire.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/13
+ */
+public class GenerateQrCodeRequire extends JobData {
+
+
+
+}
