899bca15487538e1f14209ed44e7615a5b3211f7
liumapp <liumapp.com@gmail.com>
2018-08-13 14:28:17 +0800

    update pom plugin and add spic core

---
+++ src/main/java/com/liumapp/spic/core/SpicCore.java
@@ -0,0 +1,11 @@
+package com.liumapp.spic.core;
+
+/**
+ * @author liumapp
+ * @file SpicCore.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 2018/8/13
+ */
+public class SpicCore {
+}
